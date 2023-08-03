d495b4e801b54d9e3b0fbf9373309a8e39a9fa5e
Max-von-Aiger <von.aigner@gmail.com>
2023-08-03 15:02:06 +0200

    css for recipe details page

--- src/pages/recipe/[recipeId].tsx
+++ src/pages/recipe/[recipeId].tsx
@@ -2,6 +2,7 @@ import { useRouter } from "next/router";
 import axios from "axios";
 import { useState, useEffect } from "react";
 import AddCommentForm from "@/components/AddCommentForm";
+import NavigationBar from "@/components/NavigationBar";
 
 interface Recipe {
   id: number;
@@ -51,44 +52,56 @@ const RecipesPage = () => {
   const prepTimeAsString = String(recipe.prep_time);
   return (
     <>
-      <div
-        className="recipeHero"
-        style={{
-          backgroundImage: `url(${recipe.img_url})`,
-        }}
-        title={`picture of ${recipe.name}`}
-      >
-        <h1 className="recipeH1">{recipe.name}</h1>
-        {/* <h3>{recipe.category.name}</h3> */}
-        <span>Placeholder for Recipe Categories</span>
-        <span>Placeholder for Star Rating</span>
-        <span>⭐️⭐️⭐️⭐️⭐️(hardcoded)</span>
-        <div className="blackOpacity"></div>
+      <div className="navbarDetailPage">
+        <NavigationBar />
       </div>
+      <div className="recipeHeroAndBody">
+        <div
+          className="recipeHero"
+          style={{
+            backgroundImage: `url(${recipe.img_url})`,
+          }}
+          title={`picture of ${recipe.name}`}
+        >
+          <h1 className="recipeH1">{recipe.name}</h1>
+          {/* <h3>{recipe.category.name}</h3> */}
+          <span>Placeholder for Recipe Categories</span>
+          <span>Placeholder for Star Rating</span>
+          <span>⭐️⭐️⭐️⭐️⭐️(hardcoded)</span>
+          <div className="blackOpacity"></div>
+        </div>
 
-      <div className="recipeContainer">
-        <div className="recipeContainerHeader">
-          <h2>{recipe.name}</h2>
-          <div className="recipeContainerHeaderRightPart">
-            <h2>Serves {servesAsString}</h2>
-            <h2>Prep Time {prepTimeAsString}</h2>
+        <div className="recipeContainer">
+          <div className="recipeContainerHeader">
+            <h2>{recipe.name}</h2>
+            <div className="recipeContainerHeaderRightPart">
+              <h2>Serves {servesAsString}</h2>
+              <h2>Prep Time {prepTimeAsString}</h2>
+            </div>
+          </div>
+          <div className="recipeContainerBody">
+            <div className="containerInstructions">
+              <h2>Instructions</h2>
+              <p>{recipe.instructions}</p>
+            </div>
+            <div className="containerIngredients">
+              <h2>Ingredients</h2>
+              <p>{recipe.ingredients}</p>
+            </div>
           </div>
         </div>
-        <div className="recipeContainerBody">
-          <h2>Instructions</h2>
-          <p>{recipe.instructions}</p>
-          <h2>Ingredients</h2>
-          <ul>
-            <li>{recipe.ingredients}</li>
-          </ul>
+      </div>
+
+      <div className="addCommentSection">
+        <div className="addCommentH1">
+          <h1>Add a Comment</h1>
+        </div>
+        <div className="addCommentForm">
+          <AddCommentForm />
         </div>
       </div>
-      <div className="addCommentSection">
-        <h1>Add a Comment</h1>
-        <AddCommentForm />
-      </div>
       <div className="previousCommentsSection">
-        <span>Placeholder for user comments</span>
+        <h2>CommentsPlaceholder for user comments</h2>
       </div>
     </>
   );
