5caed1a60483caf94d8737c711580868e76792f0
Max-von-Aiger <von.aigner@gmail.com>
2023-08-04 11:33:26 +0200

    styling of recipe details page

--- src/components/AddCommentForm.tsx
+++ src/components/AddCommentForm.tsx
@@ -4,20 +4,31 @@ const AddCommentForm = () => {
     const nameFromCommentForm = event.currentTarget.commenterName.value;
     const ratingFromCommentForm = event.currentTarget.rating.value;
     const reviewFromCommentForm = event.currentTarget.review.value;
+
+    console.log(nameFromCommentForm);
+    console.log(ratingFromCommentForm);
+    console.log(reviewFromCommentForm);
   };
   return (
     <>
-      <form onSubmit={handleSubmit} action="">
-        <label htmlFor="name">Name</label>
-        <input name="commenterName" id="name" type="text" />
-
-        <label htmlFor="like-peas">Rating</label>
-        <input name="rating" id="rating" type="number" />
-
-        <label htmlFor="review">Review</label>
-        <textarea name="review" id="review" />
-
-        <button type="submit">Submit</button>
+      <form onSubmit={handleSubmit} action="" className="addCommentForm">
+        <div className="addCommentFormTop">
+          <div className="commentNameField">
+            <label htmlFor="name">Name</label>
+            <input name="commenterName" id="name" type="text" />
+          </div>
+          <div className="commentRatingField">
+            <label htmlFor="rating">Rating</label>
+            <input name="rating" id="rating" type="number" />
+          </div>
+        </div>
+        <div className="commentReviewField">
+          <label htmlFor="review">Review</label>
+          <textarea name="review" id="review" />
+        </div>
+        <button className="addCommentSaveButton" type="submit">
+          Save
+        </button>
       </form>
     </>
   );
